9869d21dbaab9998cdc048495d8563bb672ce6e8
Martin Cote <martcote@gmail.com>
2022-12-02 01:08:17 -0500

    cleanup

--- day1.py
+++ day1.py
@@ -1,6 +1,5 @@
 elves = []
 totqty = 0
-curelve = 0
 maxqty = 0
 maxelve = 0
 f = open('day1.txt')
@@ -8,15 +7,8 @@ lines = f.readlines()
 for line in lines:
     if line == '\n':
         # print(f'empty {line}')
-        # elves.append([curelve, totqty])
         elves.append(totqty)
-        # print(totqty)
-        if maxqty < totqty:
-            maxqty = totqty
-            maxelve = curelve
-            
         totqty = 0
-        curelve += 1
     else:
         curqty = int(line[:-1])
         totqty += curqty
@@ -27,15 +19,8 @@ elves.append(totqty)
 print(totqty)
 if maxqty < totqty:
     maxqty = totqty
-    maxelve = curelve
 
-
-# print(elves)
-print()
-print(maxqty)
-print(maxelve)
-
-print()
 elves.sort(reverse=True)
+
 print(elves)
 print(f'top 3: {elves[0] + elves[1] + elves[2]}')
