c57cc9a1f47ad4721e0a9c4ddf6520c40aa2febc
Martin Cote <martcote@gmail.com>
2022-12-13 01:45:08 -0500

    day7 part a

--- day7.py
+++ day7.py
@@ -1,55 +1,68 @@
 # from typing import Dict
 
-commands = open('day7ex.txt').read().split('\n')
+commands = open('day7.txt').read().split('\n')
+ans = 0
             
 class Folder:
-    parent = None
-    name = ''
-    folders = {}
-    files = {}
-    # folders = Dict[str, object] = {}
-    # files: Dict[str,int] = {}
 
     def __init__(self, parent, name) -> None:
+        self.folders = {}
+        self.files = {}
+
         self.parent = parent
         self.name = name
         if parent:
             parent.folders[name] = self
 
     def get_size(self):
+        global ans
         size = 0
         for file in self.files.items():
-            print(f'name: {file[0]} size: {file[1]}')
+            # print(f'size file: {file[0]} size: {file[1]}')
             size += file[1]
 
         for folder in self.folders.items():
             size += folder[1].get_size()
 
-        print(f'name: {self.name[0]} size: {size}')
+        print(f'size folder: {self.name[0]} size: {size}')
+        if size < 100000:
+            print('ok one')
+            ans += size
+
+            
         return size
 
-root:Folder = Folder(None, '')
+root:Folder = Folder(None, '/')
 curFolder = None
 curCommand = None
 for commandLine in commands:
-    print(commandLine)
+    # print(commandLine)
     items = commandLine.split(' ')
     if items[0] == '$':
         if items[1] == 'cd':
             if items[2] == '/':
+                print(f'root {commandLine}')
                 curFolder = root
             elif items[2] == '..':
+                print(f'moveup {commandLine}')
                 curFolder = curFolder.parent
             else:
-                curFolder = curFolder.folders[items[2]]
+                foldername = items[2]
+                print(f'cd {foldername} {commandLine}')
+                curFolder = curFolder.folders[foldername]
         elif items[1] == 'ls':
             curCommand = 'ls'
     else:
         if curCommand == 'ls':
             if items[0] == 'dir':
-                Folder(curFolder, items[1])
+                name = items[1]
+                print(f'new folder {name} {commandLine}')
+                Folder(curFolder, name)
             else:
-                curFolder.files[items[1]] = int(items[0])
+                name = items[1]
+                print(f'new file {name} {commandLine}')
+                curFolder.files[name] = int(items[0])
 
 
 print(root.get_size())
+print(f'ans: {ans}')
