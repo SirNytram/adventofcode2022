873dbdbf49f8f44015d6b4aafa9d6f00b0356c7f
Martin Cote <martcote@gmail.com>
2022-12-13 22:28:06 -0500

    day 7 part 2

--- day7.py
+++ day7.py
@@ -2,7 +2,8 @@
 
 commands = open('day7.txt').read().split('\n')
 ans = 0
-            
+foundFolder = None            
+foundSize = 999999999999999
 class Folder:
 
     def __init__(self, parent, name) -> None:
@@ -14,25 +15,32 @@ class Folder:
         if parent:
             parent.folders[name] = self
 
-    def get_size(self):
-        global ans
+    def get_size(self, sizeneeded = 0):
         size = 0
         for file in self.files.items():
             # print(f'size file: {file[0]} size: {file[1]}')
             size += file[1]
 
         for folder in self.folders.items():
-            size += folder[1].get_size()
+            size += folder[1].get_size(sizeneeded)
 
+        global ans
         print(f'size folder: {self.name[0]} size: {size}')
         if size < 100000:
             print('ok one')
             ans += size
 
-            
+        global foundSize
+        global foundFolder     
+
+        if sizeneeded != 0 and size > sizeneeded and size < foundSize:
+            foundFolder = self
+            foundSize = size
+
         return size
 
 root:Folder = Folder(None, '/')
+
 curFolder = None
 curCommand = None
 for commandLine in commands:
@@ -63,6 +71,12 @@ for commandLine in commands:
                 print(f'new file {name} {commandLine}')
                 curFolder.files[name] = int(items[0])
 
+print('')
+freespace = 70000000 - root.get_size()
+neededspace = 30000000 - freespace 
+print(f'freespace {freespace} needed:{neededspace}')
+print(f'ans1: {ans}')
 
-print(root.get_size())
-print(f'ans: {ans}')
+print('')
+root.get_size(neededspace)
+print(f'folder: {foundFolder.name} size: {foundSize}')
